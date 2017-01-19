322fac738530d37b40105b11f30b011bbe9d8a8b
Thiago Raulino Dal Pont <trdp@localhost.localdomain>
2017-01-19 19:19:31 -0200

    NFC_RW -> 'Implementação da leitura de tags'

---
+++ NFCRW/app/src/main/java/br/ufsc/pibic/nfcrw/NFCWriteActivity.java
@@ -0,0 +1,13 @@
+package br.ufsc.pibic.nfcrw;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NFCWriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nfc_write);
+    }
+}
