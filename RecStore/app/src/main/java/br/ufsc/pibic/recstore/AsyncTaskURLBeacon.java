7115e629e4a58eef3e162892625e2a433c08e87b
Thiago Raulino Dal Pont <trdp@localhost.localdomain>
2017-02-06 23:10:17 -0200

    Recstore -> 'Início da implementação das URL's'

---
+++ RecStore/app/src/main/java/br/ufsc/pibic/recstore/AsyncTaskURLBeacon.java
@@ -0,0 +1,8 @@
+package br.ufsc.pibic.recstore;
+
+/**
+ * Created by trdp on 2/4/17.
+ */
+
+public class AsyncTaskURLBeacon {
+}
---
+++ RecStore/app/src/main/java/br/ufsc/pibic/recstore/Util.java
@@ -0,0 +1,8 @@
+package br.ufsc.pibic.recstore;
+
+/**
+ * Created by trdp on 2/3/17.
+ */
+
+public class Util {
+}
